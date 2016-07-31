557990357be0251d99178e9ed867772baad26c20
danyollee <leed3@oregonstate.edu>
2016-07-31 01:10:21 -0700

    scrambled the password

--- p1.cpp
+++ p1.cpp
@@ -7,14 +7,19 @@ using namespace std;
 //prototypes
 string set_username(string);
 string set_password(string, char*, int, int);
+int* scramble_password(string, char*, int, int*);
+void login(string, int*);
 
 int main(){
   string username, password;
   char *pw;
-  int length=0, trigger=0;
+  int length=0, trigger=0, *scrambled_pw;
 
   username=set_username(username);
   password=set_password(password, pw, length, trigger);
+  scrambled_pw=scramble_password(password, pw, length, scrambled_pw);
+
+  login(username, scrambled_pw);
 
   return 0;
 }
@@ -57,4 +62,30 @@ string set_password(string password, char* pw, int length, int trigger){
     }
   }
   return password;
+}
+
+int* scramble_password(string password, char* pw, int length, int* scrambled_pw){
+  length = password.length();
+  pw = new char[length];
+  scrambled_pw = new int[length];
+  strcpy(pw, password.c_str());
+
+  for(int i=0; i<length; i++){
+    scrambled_pw[i]=(((pw[i]*6)+19)/2)*16; //This is the algorithm to "encrypt" the password. Take the ASCII Dec. value of each value of the password, and then multiply it by 6, then add 19, then divide by 2, then multiply that number by 16. It's my birth day and month and then this year. 06192016.
+  }
+
+
+  return scrambled_pw;
+}
+
+void login(string username, int* scrambled_pw){
+  string login_user, login_pass;
+
+  cout << "What is your username?: ";
+  cin >> login_user;
+
+  cout << "What is your password?: ";
+  cin >> login_pass;
+
+
 }
