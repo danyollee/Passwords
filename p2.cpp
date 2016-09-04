ef4f05fbb682583bfca1783d8f316766334fa6ec
danyollee <leed3@oregonstate.edu>
2016-09-03 21:56:00 -0700

    started

---
+++ p2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h> //Includes the "getpass" function. This lets the user type without it actually being displayed on screen.
+#include <fstream>
+#include <ctime> //Includes time
+
+int main(){
+
+
+  return 0;
+}
