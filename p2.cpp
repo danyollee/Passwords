d2c86ba525e0d0718b01361b6d957bf40a35a0da
danyollee <leed3@oregonstate.edu>
2016-09-05 19:39:11 -0700

    got the alphabet cryptography thing to work.

--- p2.cpp
+++ p2.cpp
@@ -8,21 +8,59 @@
 using namespace std;
 
 //prototypes
+void fill_coders(char[][26], char[][10]);
 string set_username(string);
+string set_password(string);
 
 int main(){
   string username, password;
-  char *key;
+  char *key, codera[26][26], codernum[10][10];
 
+  fill_coders(codera, codernum);
   username=set_username(username);
-
-  cout << username << endl;
+  password=set_password(password);
 
   return 0;
+}
+
+void fill_coders(char codera[][26], char codernum[][10]){
+  char *alpha;
+  string al;
+  int length, counter=0;
+
+  al="abcdefghijklmnopqrstuvwxyz";
+  length=al.length();
+
+  strcpy(alpha, al.c_str());
+
+  for(int i=0; i<26; i++){
+    for(int j=0; j<26; j++){
+      if(i+j>25){
+        codera[i][j]=alpha[counter];
+        counter++;
+      }else{
+        codera[i][j]=alpha[j+i];
+      }
+    }
+    counter=0;
+  }
+
+  for(int i=0; i<26; i++){
+    for(int j=0; j<26; j++){
+      cout << codera[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
 
 string set_username(string username){
   cout << "Enter a username: ";
   cin >> username;
   return username;
+}
+
+string set_password(string password){
+  cout << "Enter a password: ";
+  cin >> password;
+  return password;
 }
