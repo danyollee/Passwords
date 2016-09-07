a36db1e74add285e208c45e162664db1b0f7eafc
danyollee <leed3@oregonstate.edu>
2016-09-06 17:31:14 -0700

    in the middle of copying the string password to a temp array to make a new password

--- p2.cpp
+++ p2.cpp
@@ -11,35 +11,41 @@ using namespace std;
 void fill_coders(char[][26], char[][26], char[][10]);
 string set_username(string);
 string set_password(string);
+void setup_password(string, char[], int[], char[][26], char[][26], char[][10]);
+char upper_grid(int, char[][26]);
+char lower_grid(int, char[][26]);
+char number_grid(int, char[][10]);
 
 int main(){
   string username, password;
-  char *key, coder_a[26][26], coder_A[26][26], coder_num[10][10];
+  char coder_a[26][26], coder_A[26][26], coder_num[10][10], *login_password;
+  int key[4]={4, 15, 6, 5};
 
   fill_coders(coder_a, coder_A, coder_num);
   username=set_username(username);
   password=set_password(password);
+  setup_password(password, login_password, key, coder_a, coder_A, coder_num);
 
-  for(int i=0; i<26; i++){
-    for(int j=0; j<26; j++){
-      cout << coder_a[i][j] << " ";
-    }
-    cout << endl;
-  }
-
-  for(int i=0; i<26; i++){
-    for(int j=0; j<26; j++){
-      cout << coder_A[i][j] << " ";
-    }
-    cout << endl;
-  }
-
-  for(int i=0; i<10; i++){
-    for(int j=0; j<10; j++){
-      cout << coder_num[i][j] << " ";
-    }
-    cout << endl;
-  }
+  // for(int i=0; i<26; i++){
+  //   for(int j=0; j<26; j++){
+  //     cout << coder_a[i][j] << " ";
+  //   }
+  //   cout << endl;
+  // }
+  //
+  // for(int i=0; i<26; i++){
+  //   for(int j=0; j<26; j++){
+  //     cout << coder_A[i][j] << " ";
+  //   }
+  //   cout << endl;
+  // }
+  //
+  // for(int i=0; i<10; i++){
+  //   for(int j=0; j<10; j++){
+  //     cout << coder_num[i][j] << " ";
+  //   }
+  //   cout << endl;
+  // }
 
   return 0;
 }
@@ -107,4 +113,43 @@ string set_password(string password){
   cout << "Enter a password: ";
   cin >> password;
   return password;
+}
+
+void setup_password(string password, char login_password[], int key[], char coder_A[][26], char coder_a[][26], char coder_num[][10]){
+  int length, counter=0, num_counter=3;
+  char *temp;
+
+  temp=new char[length];
+  strcpy(temp, password.c_str());
+
+  length=password.length();
+  login_password=new char[length];
+
+  for(int i=0; i<length; i++){
+    if(counter>2){
+      counter=0;
+    }
+    if(temp[i]>=65 && temp[i]<=90){
+      login_password[i]=upper_grid(counter, coder_A);
+    }
+    if(temp[i]>=97 && temp[i]<=122){
+      login_password[i]=lower_grid(counter, coder_a);
+    }
+    if(temp[i]>=48 && temp[i]<=57){
+      login_password[i]=number_grid(num_counter, coder_num);
+    }
+  }
+
+}
+
+char upper_grid(int counter, char coder_A[][26]){
+
+}
+
+char lower_grid(int counter, char coder_a[][26]){
+
+}
+
+char number_grid(int counter, char coder_num[][10]){
+
 }
